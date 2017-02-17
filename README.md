1515a1a6de3c38bb22d7979a4a7620b2e1fe0a19
stephanec <stephanec@washingmachine>
2017-02-17 23:12:16 +0100

    review

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Package visitedlink helps reading chromium Visited Links.
+# visitedlink
+
+Command visitedlink helps reading chromium Visited Links.
+
+Prints each url on stdout if visited or else on stderr.
+
+> Usage:
+
+```
+visitedlink <Visited Link file> urls...
+```
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
-// Package visitedlink helps reading chromium Visited Links.
+// Command visitedlink helps reading chromium Visited Links.
+//
+//  Usage:
+//  visitedlink <Visited Link file> urls...
+// Prints each url on stdout if visited or else on stderr.
 package main
 
 import (
 	"crypto/md5"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path"
@@ -27,10 +32,19 @@ type fileHeader struct {
 func main() {
 	log.SetFlags(0)
 
+	if len(os.Args) < 3 {
+		log.Println(`Usage:
+	visitedlink <Visited Link file> urls...
+
+Prints each url on stdout if visited or else on stderr.`)
+		os.Exit(2)
+	}
+
+	// open file
 	name := path.Clean(os.Args[1])
 	file, err := os.Open(name)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Unable to open %q: %v", name, err)
 	}
 	defer file.Close()
 
@@ -38,11 +52,16 @@ func main() {
 	header := new(fileHeader)
 	err = binary.Read(file, binary.LittleEndian, header)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Unable to read %q: %v", name, err)
 	}
-	verifyHeader(file, header)
 
-	// search urls
+	// verify header
+	err = verifyHeader(file, header)
+	if err != nil {
+		log.Fatalf("Bad header: %v", err)
+	}
+
+	// search for urls
 	for i := 2; i < len(os.Args); i++ {
 		url := os.Args[i]
 		fp := fingerprint(url, header.Salt)
@@ -56,34 +75,44 @@ func main() {
 	}
 }
 
-func verifyHeader(file *os.File, header *fileHeader) {
-	if fileSignature != header.Signature {
-		log.Fatal("bad signature")
+func verifyHeader(file *os.File, header *fileHeader) error {
+	if header.Signature != fileSignature {
+		return fmt.Errorf("bad signature: %x, want: %x", header.Signature, fileSignature)
 	}
-	if fileVersion != header.Version {
-		log.Fatal("bad version")
+	if header.Version != fileVersion {
+		return fmt.Errorf("bad version: %x, want: %x", header.Version, fileVersion)
 	}
-	if header.Used > header.Length {
-		log.Fatal("bad used count")
+
+	stat, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("unable to stat: %v", err)
 	}
 
 	size := int64(header.Length*8 + headerSize)
-	stat, _ := file.Stat()
-	if size != stat.Size() {
-		log.Fatal("bad file size")
+	if stat.Size() != size {
+		return fmt.Errorf("bad file size: %x, want: %x", stat.Size(), size)
 	}
+
+	return nil
 }
 
-func isVisited(file *os.File, fp uint64, fpm int32) bool {
+func isVisited(file io.ReadSeeker, fp uint64, fpm int32) bool {
 	pos := headerSize + fpm*8
-	_, err := file.Seek(int64(pos), 0)
+	_, err := file.Seek(int64(pos), io.SeekStart)
+	if err != nil {
+		return false
+	}
 
-	for fpc := uint64(0); err == nil; {
+	var fpc uint64
+	for {
 		err = binary.Read(file, binary.LittleEndian, &fpc)
+		if err != nil {
+			return false
+		}
 
 		if fpc == 0 {
 			break
-		} else if fp == fpc {
+		} else if fpc == fp {
 			return true
 		}
 	}
@@ -93,13 +122,15 @@ func isVisited(file *os.File, fp uint64, fpm int32) bool {
 
 func fingerprint(url string, salt [8]uint8) uint64 {
 	hash.Reset()
+
+	// ignore errors as hash/Hash.Writer never returns an error
 	hash.Write(salt[:])
 	hash.Write([]byte(url))
 
 	// sum is [16]byte
 	sum := hash.Sum(nil)
 
-	// uses the top 64 bits
+	// use the top 64 bits
 	return binary.LittleEndian.Uint64(sum[:8])
 }
 
